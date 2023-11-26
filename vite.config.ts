260236166fae61fdf24fb684e5796c8800f3c878
John Yuan <john.ycq@outlook.com>
2023-11-26 19:40:23 +0800

    feat: add src alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
+import url from 'node:url'
+import path from 'node:path'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(path.dirname(url.fileURLToPath(import.meta.url)), 'src')
+    }
+  }
 })
